8e41f0a0cf46008ecb1e50a0f6913d062231c550
Chayannn <chayanp188@gmail.com>
2023-05-07 10:13:40 +0530

    Test

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
-// import http from 'http';
-// import { generateLovePercent } from './features.js';
-// import fs from 'fs';
-
-// const home = fs.readFileSync('./index.html');
-
-// const server = http.createServer((req, res) => {
-//   if (req.url === '/about') {
-//     res.end(`<h1>Love is ${generateLovePercent()}</h1>`);
-//   } else if (req.url === '/') {
-//     res.end(home);
-//   } else if (req.url === '/contact') {
-//     res.end('<h1>Contact Page</h1>');
-//   } else {
-//     res.end('<h1>Page not found</h1>');
-//   }
-// });
-
-// server.listen(3000, () => {
-//   console.log('Server is working');
-// });
 import express from 'express';
-import fs from 'fs';
+import path from 'path';
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(path.resolve(), 'public'));
 
 app.get('/', (req, res) => {
-  const file = fs.readFileSync('./index.html');
-  res.sendFile(file);
+  const pathlocation = path.resolve();
+  res.render('index', { name: 'gar marche' });
 });
 
 app.listen(3000, () => {
