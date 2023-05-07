9c44586b12e3725d4480936dc4594826fbbf7ba1
Chayannn <chayanp188@gmail.com>
2023-05-07 10:21:33 +0530

    fixed a error

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from 'express';
 import path from 'path';
 
 const app = express();
+
+app.use(express.static(path.join(path.resolve(), 'public')));
+
 app.set('view engine', 'ejs');
-app.use(express.static(path.resolve(), 'public'));
 
 app.get('/', (req, res) => {
-  const pathlocation = path.resolve();
-  res.render('index', { name: 'gar marche' });
+  res.render('index', { name: 'Chayan' });
 });
 
 app.listen(3000, () => {
