cf3cd198a14728ad08ede64c8f340d97acb29323
Chayannn <chayanp188@gmail.com>
2023-05-07 18:02:52 +0530

    updated

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ import path from 'path';
 const app = express();
 
 app.use(express.static(path.join(path.resolve(), 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+const users = [];
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index', { name: 'Chayan' });
 });
+app.get('/success', (req, res) => {
+  res.render('success');
+});
+
+app.post('/contact', (req, res) => {
+  users.push({ username: req.body.name, email: req.body.email });
+  res.redirect('/success');
+});
+
+app.get('/users', (req, res) => {
+  res.json({
+    users,
+  });
+}); 
 
 app.listen(3000, () => {
   console.log('Sever is running');
