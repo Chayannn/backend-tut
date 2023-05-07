b602f6c3d5a81538e6d6d2e47c811098fd823259
Chayannn <chayanp188@gmail.com>
2023-05-07 23:54:03 +0530

    updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
 import { name } from 'ejs';
+import cookieParser from 'cookie-parser';
 
 mongoose
   .connect('mongodb://127.0.0.1:27017', {
@@ -21,13 +22,36 @@ const app = express();
 
 app.use(express.static(path.join(path.resolve(), 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const users = [];
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', { name: 'Chayan' });
+  const { token } = req.cookies;
+  if (token) {
+    res.render('logout');
+  } else {
+    res.render('login');
+  }
+});
+
+app.post('/login', (req, res) => {
+  res.cookie('token', 'iamin', {
+    httpOnly: true,
+    expires: new Date(Date.now() + 60 * 1000),
+  });
+
+  res.redirect('/');
+});
+
+app.get('/logout', (req, res) => {
+  res.cookie('token', null, {
+    expires: new Date(Date.now()),
+  });
+
+  res.redirect('/');
 });
 
 app.get('/add', async (req, res) => {
@@ -46,6 +70,7 @@ app.get('/success', (req, res) => {
 app.post('/contact', async (req, res) => {
   const { name, email } = req.body;
   await Message.create({ name: name, email: email });
+
   res.redirect('/success');
 });
 
